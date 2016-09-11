10776b27e63c8f93e3e540e744e9c119753048be
Zandt Tittle <mace01@gmail.com>
2016-09-11 14:14:41 -0500

    Add rdfa to Event

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 // See context at http://www.markus-lanthaler.com/hydra/event-api/contexts/Event.jsonld
 const Event = props =>
     <div>
-        <h1>{props.instance['http://schema.org/name'][0]['@value']}</h1>
-        <h2>{props.instance['http://schema.org/description'][0]['@value']}</h2>
+        <h1 property="http://schema.org/name" value={props.instance['http://schema.org/name'][0]['@value']}>{props.instance['http://schema.org/name'][0]['@value']}</h1>
+        <h2 property="http://schema.org/description" value={props.instance['http://schema.org/description'][0]['@value']}>{props.instance['http://schema.org/description'][0]['@value']}</h2>
     </div>;
 
 Event.propTypes = {
